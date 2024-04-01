2415eed544a76f9cf418a1490b5f77cd0dfc8ee6
RoseHobgoblin <earlofmagadan@Protonmail.com>
2024-04-02 03:37:08 +1300

    burakku

--- smartvector.h
+++ smartvector.h
@@ -870,8 +870,13 @@ public:
     }
 
 private:
-    void _assign(std::initializer_list<value_base_type> &il)
-    { _assign(il.begin(), il.end()); }
+    template <class InputIterator>
+    void _assign(InputIterator first, InputIterator last) {
+        base.clear();  // Clear existing elements
+        base.reserve(std::distance(first, last)); // Pre-allocate for efficiency
+        for (auto it = first; it != last; ++it) {
+            base.emplace_back(std::make_unique<T>(*it)); // Construct in-place
+        }
 
 
     // Available if the passed iterator references an object by pointer and not by value.
